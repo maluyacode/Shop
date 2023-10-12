548384751ccc56b38d84b0ae8065ea2af09af941
maluyacode <adlawandavemerc98@gmail.com>
2023-10-12 09:32:43 +0800

    frontend-setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Layout/Header'
+import Footer from './Components/Layout/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
