4d465bac9e4f9d4ec5a21e8fdccce359b1df157f
maluyacode <adlawandavemerc98@gmail.com>
2023-10-12 09:41:02 +0800

    home js

--- frontend/src/Components/Layout/Header.js
+++ frontend/src/Components/Layout/Header.js
@@ -29,6 +29,7 @@ const Header = () => {
 
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
                     <button className="btn" id="login_btn">Login</button>
+
                     <span id="cart" className="ml-3">Cart</span>
                     <span className="ml-1" id="cart_count">2</span>
                 </div>
