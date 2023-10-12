d2cf59b19d41a1992e8f9d029ba101da1a22dcc1
maluyacode <adlawandavemerc98@gmail.com>
2023-10-12 08:48:47 +0800

    order controller

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
 const cookieParser = require('cookie-parser')
+
+const products = require('./routes/product');
+const auth = require('./routes/auth');
+
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-const products = require('./routes/product');
-const auth = require('./routes/auth');
+
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
