14f8103bc8fb790ffc077a40106714ff0cffb00e
maluyacode <116046282+maluyacode@users.noreply.github.com>
2024-01-17 10:32:55 +0800

    cart functions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { legacy_createStore as createStore, combineReducers, applyMiddleware, co
 import { thunk } from 'redux-thunk'
 import { productsReducer, productDetailsReducer, newProductReducer } from './reducers/productReducers';
 import { authReducer, userReducer, forgotPasswordReducer } from './reducers/userReducers';
-
+import { cartReducer } from './reducers/cartReducers'
 const reducer = combineReducers({
   products: productsReducer,
   productDetails: productDetailsReducer,
@@ -11,12 +11,23 @@ const reducer = combineReducers({
   auth: authReducer,
   user: userReducer,
   forgotPassword: forgotPasswordReducer,
+  cart: cartReducer,
 })
 
 
 let initialState = {
-
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+    shippingInfo: localStorage.getItem('shippingInfo')
+      ? JSON.parse(localStorage.getItem('shippingInfo'))
+      : {}
+  },
+  user: {},
+  auth: {},
 }
+
 const middleware = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
